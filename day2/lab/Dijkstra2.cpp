a03565e1a4df2afbba628a28cb290eb1db1508b4
Gagik Hakobyan <gagik.hakobyan.external@worldline.com>
2016-10-11 14:52:53 +0400

    Arman's version of Dining philosophers problem.

---
+++ day2/lab/Dijkstra2.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	HANDLE x0 = OpenMutex(0, 0, L"left");
+	HANDLE x1 = OpenMutex(0, 0, L"right");
+
+	if (x0 == NULL) {
+		x0 = CreateMutex(0, 0, L"left");
+	}
+	if (NULL == x1) {
+		x1 = CreateMutex(0, 0, L"right");
+	}
+
+	while (true) {
+		cout << "Waiting for forks" << endl;
+		WaitForSingleObject(x0, INFINITE); // take left fork
+		WaitForSingleObject(x1, INFINITE); // take right fork
+		std::cout << "1 - start eating" << std::endl;
+		Sleep(10); // eat
+
+		ReleaseMutex(x0); // take down
+		ReleaseMutex(x1); // take down
+		std::cout << "0 - start thinking" << std::endl;
+
+		Sleep(100); // think
+	}
+	return 0;
+}
---
+++ day2/lab/readme.txt
@@ -0,0 +1,5 @@
+Dining philoshopers problem implementation with 2 process.
+
+Note, We could not pass the philosopher ID to the running process yet :)
+
+Author: Arman, K43
